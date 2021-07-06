e2e8841060c0e9fc9ee53dc9d2aae425e296e937
Johncarter49 <81585622+Johncarter49@users.noreply.github.com>
2021-07-06 15:18:07 +0200

    these works are for loops

---
+++ 04-Döngüler/dongu.js
@@ -0,0 +1,69 @@
+
+
+//  Loops  //
+
+// For Loops //
+
+
+/*
+for (var i=0; i<=10 ; i++){
+    console.log(i);
+}
+*/
+
+/* for (var i=0; i<=100 ; i++){
+    if(i%2==0) {
+        console.log(i);
+     }
+    }
+*/
+
+/*
+for (var i=0; i<=100 ; i++){
+    if(i==50) {
+        break;
+    }
+    console.log(i);
+}
+*/
+
+for (var i=0; i<=100 ; i++){
+    if(i==50) {
+        continue;
+    }
+    console.log(i);
+}
+
+/*
+var cities = ["Washington", "Istanbul", "New_york", "Boston"]
+
+for(var i=0; i<cities.length; i++){
+    console.log(cities[i]);
+
+}
+
+*/
+
+// While Loops //
+
+/*
+var i=1;
+
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+*/
+
+/* 
+
+var i=0;
+var cities = ["Washington", "Istanbul", "New_york", "Boston"]
+
+while(i<cities.length){
+    console.log(cities[i]);
+    i++;
+}
+
+*/
