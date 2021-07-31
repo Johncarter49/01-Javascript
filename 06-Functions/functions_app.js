3cadcce807a770d5e017ef1793e2ed4f82fc95bb
Johncarter49 <81585622+Johncarter49@users.noreply.github.com>
2021-07-31 16:01:34 +0200

    functions_app

---
+++ 06-Functions/functions_app.js
@@ -0,0 +1,72 @@
+
+
+/*
+#1 : Please write function that calculate whether the input is odd or even numbers.
+
+function oddEven(number) {
+    if (number % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+if (oddEven(30)) {
+    console.log('number is odd.');
+} else {
+    console.log('number is even.');
+}
+
+let number = oddEven(29);
+console.log(number);
+
+#2 : Please write function that calculate factorial of  input.
+
+
+function factorial(number){
+    let result = 1;
+
+    for(let i=2; i<=number; i++){
+        result *=i;
+    }
+    return result;
+}
+
+console.log(factorial(4));
+#3 : Please write function that write every elements of the given Arrays.
+
+
+let models = ['opel', 'model','toyota'];
+
+function print(array){
+    array.forEach(item => {
+        console.log(item);
+    });
+}
+print(models);
+
+*/
+
+
+
+
+/*
+#1:
+
+let number = 12;
+
+function oddEvenCalculate(number){
+    let odd = number % 2;
+
+    if(odd == 0){
+        console.log(number + ' is odd number. ');
+    }else{
+        console.log(number + ' is even number.')
+    }
+
+}
+*/
+
+
+
